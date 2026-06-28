6b015d527e421c607d6931c0a270085fb7b743d5
Youssef Zaoujal <youssef.zaoujal@ecole.ensicaen.fr>
2026-06-29 01:24:59 +0200

    chore: configure production settings for deployment

--- backend/yoha/settings/production.py
+++ backend/yoha/settings/production.py
@@ -10,3 +10,14 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+ALLOWED_HOSTS = [
+    "94.237.99.114",
+    "localhost",
+]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+CORS_ALLOWED_ORIGINS = [
+    "http://94.237.99.114",
+]
+
